3093d30e6f76ce6b8841ba0cf464ccb8449e8dc2
welt113 <82703022+welt113@users.noreply.github.com>
2021-04-18 01:53:14 +0000

    Update and rename Is.md to Python2 welt.py

---
+++ Python2 welt.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m Dennis Kwarteng
+- 👀 I’m interested in Software engineering
+- 🌱 I’m currently learning Basics Software
+- 💞️ I’m looking to collaborate on GitHub
+- 📫 How to reach me +233544968687
+
+<!---
+welt113/welt113 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
